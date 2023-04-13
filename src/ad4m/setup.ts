a4d99f378b6bf620bc9917098528df586960c344
Wojtek Gorecki <hallo@wojtek-gorecki.de>
2023-04-13 10:43:21 +0200

    Functions for setting up ad4m

---
+++ src/ad4m/setup.ts
@@ -0,0 +1,40 @@
+import type { Ad4mClient, PerspectiveProxy } from "@perspect3vism/ad4m";
+import { LinkQuery } from "@perspect3vism/ad4m";
+import Ad4mConnectUI from '@perspect3vism/ad4m-connect';
+
+import { PERSPECTIVE_NAME, PROJECT_ID, PROJECT_NAME } from "../constants";
+
+export async function connectToAd4m(): Promise<Ad4mClient> {
+  const ui = Ad4mConnectUI({
+    appName: "AD4M React",
+    appDesc: "AD4M - React Test App",
+    appDomain: "dev.ad4m.react-test-app",
+    capabilities: [{ with: { domain: "*", pointers: ["*"] }, can: ["*"] }],
+    appIconPath: "https://i.ibb.co/GnqjPJP/icon.png",
+  });
+
+  return ui.connect();
+}
+
+export async function ensurePerspectiveAndProject(ad4mClient: Ad4mClient): Promise<PerspectiveProxy> {
+
+  // ensure perspective
+  const perspectives = await ad4mClient?.perspective.all();
+  let perspective = perspectives.find(p => p.name === PERSPECTIVE_NAME)
+
+  if (!perspective) {
+    perspective = await ad4mClient.perspective.add(PERSPECTIVE_NAME);
+  }
+
+  // ensure project
+  const queryResults = await perspective.get(new LinkQuery({ source: PROJECT_ID }));
+  if (!queryResults.length) {
+    const link = await perspective.add({
+      source: PROJECT_ID,
+      predicate: "project://has_name",
+      target: `literal://string:${encodeURI(PROJECT_NAME)}`,
+    });
+  }
+
+  return perspective;
+}
