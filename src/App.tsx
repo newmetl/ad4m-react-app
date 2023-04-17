26f939c170017d75315fc0e18a6dbe80d523a89d
Wojtek Gorecki <hallo@wojtek-gorecki.de>
2023-04-17 11:08:39 +0200

    dry up code and remove console logs

--- src/App.tsx
+++ src/App.tsx
@@ -12,36 +12,32 @@ import TodoList from './components/TodoList';
 import CreateTodo from './components/CreateTodo';
 
 import './App.css';
+import { PerspectiveProxy } from '@perspect3vism/ad4m';
+
+function usingAd4m(): Promise<PerspectiveProxy> {
+  return getAd4mClient().then((client) => ensurePerspectiveAndProject(client));
+}
 
 function App() {
-  console.log('--> render()');
-
   const [isConnected, setIsConnected] = useState(false);
   const [isLoadingState, setIsLoadingState] = useState(false);
-
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const loadTodosFromPerspective = () => {
-    console.log('--> loadTodosFromPerspective()');
-
     setIsLoadingState(true);
 
-    getAd4mClient().then((client) => {
-      ensurePerspectiveAndProject(client).then((perspective) => {
-        fetchTodos(perspective, PROJECT_ID)
-          .then((todos) => {
-            setTodos(todos);
-            setIsLoadingState(false);
-          });
+    usingAd4m().then((perspective) => {
+      fetchTodos(perspective, PROJECT_ID)
+      .then((todos) => {
+        setTodos(todos);
+        setIsLoadingState(false);
       });
     });
   }
 
+  // Initial side effect. Connect to AD4M, ensure perspective and project, load todos from ad4m.
   useEffect(() => {
-    console.log('--> useEffect()');
-
     if (!isConnected) {
-      console.log('-- --> ad4m not connected. Connecting now ...');
       connectToAd4m().then((client) => {
         ensurePerspectiveAndProject(client).then(() => {
           loadTodosFromPerspective();
@@ -52,55 +48,46 @@ function App() {
 
 	}, []);
 
-  const onClickTodoItem = (todoId: string) => {
-    console.log('--> onClickTodoItem');
+  const handleClickTodoItem = (todoId: string) => {
     setIsLoadingState(true);
-    getAd4mClient().then((client) => {
-      ensurePerspectiveAndProject(client).then((perspective) => {
-        const todo = todos.find((todo) => todo.id === todoId);
-        if (todo) {
-          const newTodo = {
-            ...todo,
-            state: !todo.state
-          }
-          updateTodo(perspective, newTodo).then(() => {
-            loadTodosFromPerspective();
-          });
+
+    usingAd4m().then((perspective) => {
+      const todo = todos.find((todo) => todo.id === todoId);
+      if (todo) {
+        const newTodo = {
+          ...todo,
+          state: !todo.state
         }
-      });
-    });
-  }
-
-  const onCreateNewTodo = (text: string) => {
-    console.log('--> onCreateNewTodo()', text);
-    setIsLoadingState(true);
-
-    getAd4mClient().then((client) => {
-      ensurePerspectiveAndProject(client).then((perspective) => {
-        createTodo(perspective, PROJECT_ID, text, false).then(() => {
+        updateTodo(perspective, newTodo).then(() => {
           loadTodosFromPerspective();
         });
-      });
+      }
     });
-
   }
 
-  const onRemoveTodo = (todoId: string) => {
-    console.log('--> onRemoveTodo()', todoId);
+  const handleCreateNewTodo = (text: string) => {
+    setIsLoadingState(true);
+
+    usingAd4m().then((perspective) => {
+      createTodo(perspective, PROJECT_ID, text, false).then(() => {
+        loadTodosFromPerspective();
+      });
+    });
+  }
+
+  const handleRemoveTodo = (todoId: string) => {
     setIsLoadingState(true);
 
     const todo: Todo | undefined = todos.find((item) => item.id === todoId);
 
     if (todo) {
-      getAd4mClient().then((client) => {
-        ensurePerspectiveAndProject(client).then((perspective) => {
-          deleteTodo(perspective, PROJECT_ID, todo).then(() => {
-            loadTodosFromPerspective();
-          });
+      usingAd4m().then((perspective) => {
+        deleteTodo(perspective, PROJECT_ID, todo).then(() => {
+          loadTodosFromPerspective();
         });
       });
     } else {
-      console.log('Delete failed. Cannot find todo with id:', todoId);
+      console.error('Delete failed. Cannot find todo with id:', todoId);
     }
 
   }
@@ -110,10 +97,10 @@ function App() {
       <header className="App-header">
         <TodoList
           disabled={isLoadingState}
-          onTodoItemClick={onClickTodoItem}
-          onRemove={onRemoveTodo}
+          onTodoItemClick={handleClickTodoItem}
+          onRemove={handleRemoveTodo}
           data={todos} />
-        <CreateTodo onSubmit={onCreateNewTodo} />
+        <CreateTodo onSubmit={handleCreateNewTodo} />
       </header>
     </div>
   );
